a5f770d819845fec938d9c535552952866f1805b
fdudaev <fdudaev@gmail.com>
2023-04-27 19:26:19 -0400

    push from iMac

--- src/day01_introToProgramming/MainClass.java
+++ src/day01_introToProgramming/MainClass.java
@@ -1,2 +1,9 @@
 package day01_introToProgramming;public class MainClass {
+
+    public static void main(String[] args) {
+        int myNum = 4;
+        for (int i = 1; i <myNum; i++){
+            System.out.println("num = " + i);
+        }
+    }
 }
